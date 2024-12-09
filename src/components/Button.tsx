0a2ac19a07269c395128218b6bc3f1dc2dd196ef
DocSeattle <seattledraws@gmail.com>
2024-12-09 12:13:16 +0100

    +button +button styles +button props *need globals

---
+++ src/components/Button.tsx
@@ -0,0 +1,35 @@
+import './Button.css';
+
+interface ButtonProps {
+    text: String;
+    linkTo: String;
+    onClick: Function;
+}
+
+/* 
+* @remarks
+* A function to create an Anchor element styled as a button, with certain properties set.
+* 
+* @param Text - Visual text of the button
+* @param LinkTo - Sets the href of the button
+* @param OnClick - the onClick function of the button
+*
+* @returns A button with above parameters.
+* 
+* @beta
+*/
+export default function Button({text, linkTo, onClick}: ButtonProps) {
+
+
+    return (
+        <a href={linkTo}
+        onClick={onClick}
+        className="
+            own-Button
+            max-w-24
+            max-h-8
+        ">
+        {text == null || "" ? "TEXT IS NULL"  : text}
+        </a>
+    )
+}
