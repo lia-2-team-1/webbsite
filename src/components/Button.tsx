57c9cc5881a15e2c00a216a9032ee3f4b1bf4bce
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2024-12-13 15:05:56 +0100

    WIP: Local changes for hero section again

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,28 +1,27 @@
 interface ButtonProps {
-    text: String;
-    linkTo: String;
-    onClick: Function;
+  text: String;
+  linkTo: String;
+  onClick: Function;
 }
 
-/* 
-* @remarks
-* A function to create an Anchor element styled as a button, with certain properties set.
-* 
-* @param Text - Visual text of the button
-* @param LinkTo - Sets the href of the button
-* @param OnClick - the onClick function of the button
-*
-* @returns A button with above parameters.
-* 
-* @beta
-*/
-export default function Button({text, linkTo, onClick}: ButtonProps) {
-
-
-    return (
-        <a href={linkTo}
-        onClick={onClick}
-        className="
+/*
+ * @remarks
+ * A function to create an Anchor element styled as a button, with certain properties set.
+ *
+ * @param Text - Visual text of the button
+ * @param LinkTo - Sets the href of the button
+ * @param OnClick - the onClick function of the button
+ *
+ * @returns A button with above parameters.
+ *
+ * @beta
+ */
+export default function Button({ text, linkTo, onClick }: ButtonProps) {
+  return (
+    <a
+      href={linkTo}
+      onClick={onClick}
+      className="
             font-bold font-inter text-base uppercase
             py-2 px-10
             my-1 mx-3
@@ -34,10 +33,9 @@ export default function Button({text, linkTo, onClick}: ButtonProps) {
             disabled:bg-[#888888] disabled:text-black
             visited:text-black
             transition-colors
-            max-w-24
-            max-h-8
-        ">
-        {text == null || "" ? "TEXT IS NULL"  : text}
-        </a>
-    )
+        "
+    >
+      {text == null || "" ? "TEXT IS NULL" : text}
+    </a>
+  );
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,12 +3,15 @@ import Button from "./Button";
 
 export const Hero = () => {
   return (
-    <div>
+    <div className="w-full overflow-x-hidden">
       <div
-        className="min-h-screen bg-center bg-cover bg-blend-overlay bg-fixed"
-        style={{ backgroundImage: "url('/tif-hero.jpg')" }}
+        className="min-h-screen bg-center bg-cover bg-blend-overlay overflow-x-scroll" 
+        style={{ backgroundImage: "url('/tif-hero-small.jpg')" }}
       >
-        <h1>Hej</h1>
+        <div className="bg-black p-10 bg-opacity-70 flex flex-col absolute left-20 bottom-0 items-center pt-20">
+            <Button onClick={''} linkTo={'#'} text={'STÖTTA KLUBBEN'}></Button>
+            <Button onClick={''} linkTo={'#'} text={'BLI MEDLEM'}></Button>
+        </div>
       </div>
     </div>
   );
