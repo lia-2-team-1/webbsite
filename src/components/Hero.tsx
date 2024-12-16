e95ef45adc1f2fc24d26596189f28b39cd17df7c
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2024-12-16 11:51:38 +0100

    hero stuff

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,12 +5,15 @@ export const Hero = () => {
   return (
     <div className="w-full overflow-x-hidden">
       <div
-        className="min-h-screen bg-center bg-cover bg-blend-overlay overflow-x-scroll" 
-        style={{ backgroundImage: "url('/tif-hero-small.jpg')" }}
+        className="overflow-x-hidden min-h-screen bg-center bg-cover bg-no-repeat 
+        lg:bg-[url('/tif-hero.jpg')]
+        bg-[url('/hero-mobile.jpg')]
+        // " 
+        // style={{ backgroundImage: "url('/tif-hero.jpg')" }}
       >
-        <div className="bg-black p-10 bg-opacity-70 flex flex-col absolute left-20 bottom-0 items-center pt-20">
-            <Button onClick={''} linkTo={'#'} text={'STÖTTA KLUBBEN'}></Button>
-            <Button onClick={''} linkTo={'#'} text={'BLI MEDLEM'}></Button>
+        <div className="bg-black p-10 bg-opacity-70 flex flex-col lg:left-80 absolute lg:-bottom-14 items-center">
+            <Button onClick={''} linkTo={'#'} text={'DONERA'}></Button>
+            <Button onClick={''} linkTo={'#'} text={'GÅ MED'}></Button>
         </div>
       </div>
     </div>
