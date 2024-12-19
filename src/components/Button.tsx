2f57dd414cdc6b643c92cd579a790ee00fe76293
DocSeattle <seattledraws@gmail.com>
2024-12-19 13:59:03 +0100

    +cleaned up styles. +Width param works

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -28,54 +28,50 @@ export default function Button({text, isLink, linkTo, func, width}: ButtonProps)
         if (isLink) {
             return (
                 <>
-                <div className={`
-                py-1 px-2
-                my-1 mx-3
-                border-4 rounded-md border-transparent
-                bg-sandybrown text-black 
-                hover:bg-[#3D3D3D] hover:text-springwood
-                focus:bg-[#3D3D3D] focus:text-springwood
-                focus:border-sandybrown
-                active:bg-[#B0B0B0] active:text-black
-                disabled:bg-[#888888] disabled:text-black
-                transition-colors
-                w-${!width ? "fit" : width} 
-                `}>
-                        <a href={`${linkTo}`} className="
-                            font-bold font-inter uppercase
-                            text-black
-                            visited:text-black
-                            text-center 
-                        ">
-                        {`${text}`}
-                        </a>
-                  </div>
+                <a href={`${linkTo}`} className={`
+                    inline-block
+                    font-bold font-inter uppercase
+                    py-1 px-2
+                    my-1 mx-3
+                    bg-sandybrown text-black
+                    border-4 rounded-md border-transparent
+                    transition-colors text-center
+
+                    hover:bg-[#3D3D3D] hover:text-springwood hover:border-sandybrown
+                    focus:bg-[#3D3D3D] focus:text-springwood focus:border-sandybrown
+                    focus:outline-0 focus:outline-offset-3 focus:outline-sandybrown
+                    active:bg-[#B0B0B0] active:text-black
+                    disabled:bg-[#888888] disabled:text-black
+                    visited:text-black
+                    w-
+                    w-${!width ? "fit" : width}
+                    `}>
+                    {`${text}`}
+                    </a>
                 </>
             )
         }
         else {
             return(
-                <>
                     <button type="button"
                     className={`
                     font-bold font-inter uppercase
-                    visited:text-black
                     py-1 px-2
                     my-1 mx-3
-                    border-4 rounded-md border-transparent
                     bg-sandybrown text-black
-                    hover:bg-[#3D3D3D] hover:text-springwood
-                    focus:bg-[#3D3D3D] focus:text-springwood
-                    focus:border-sandybrown
+                    border-4 rounded-md border-transparent
+                    transition-colors text-center
+
+                    hover:bg-[#3D3D3D] hover:text-springwood hover:border-sandybrown
+                    focus:bg-[#3D3D3D] focus:text-springwood focus:border-sandybrown
+                    focus:outline-0 focus:outline-offset-3 focus:outline-sandybrown
                     active:bg-[#B0B0B0] active:text-black
                     disabled:bg-[#888888] disabled:text-black
-                    transition-colors
-                    text-center 
+                    visited:text-black
                     w-${!width ? "fit" : width}
                     `}
 
                     >{`${text}`}</button>
-                </>
             )
         }
 }
