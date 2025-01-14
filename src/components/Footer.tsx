45daaccf02f3395f6f720951c7f28da66f41c8be
DocSeattle <seattledraws@gmail.com>
2025-01-14 15:16:16 +0100

    +boilerplate

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+interface FooterProps {
+
+}
+
+export default function Footer({}: FooterProps) {
+    return(
+        <>
+            <footer
+                className="
+                    flex
+                "
+            >
+                
+            </footer>
+        </>
+    )
+}
