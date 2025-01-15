8dfd52688d43496c12773d696d6605d8cd989a47
Karin Ekenberg <137770952+karinEkenberg@users.noreply.github.com>
2025-01-15 09:19:01 +0100

    styled and ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import SponsorSection from './components/SponsorSection.tsx'
-import './index.css'
-import Button from './components/Button.tsx' // *
-import Hero from './components/Hero.tsx'
-import Navbar from './components/Navbar.tsx'
-import News from './components/News.tsx'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import SponsorSection from "./components/SponsorSection.tsx";
+import "./index.css";
+import Button from "./components/Button.tsx"; // *
+import Hero from "./components/Hero.tsx";
+import Navbar from "./components/Navbar.tsx";
+import News from "./components/News.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div className="w-screen">
+      <div className="w-screen">
         <h1 className="text-center">Centered?</h1>
-    </div>
+      </div>
       <div>
-        <Navbar></Navbar>
-        <Hero></Hero>
-        <News></News>
-       <SponsorSection></SponsorSection>
+        <Navbar />
+        <Hero />
+        <News />
+        <SponsorSection />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/NavLinks.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const NavLinks = ({ className }) => {
+  const links = [
+    { href: "#", text: "Hem" },
+    { href: "#", text: "Om oss" },
+    { href: "#", text: "Schema" },
+    { href: "#", text: "Nyheter" },
+    { href: "#", text: "Tider" },
+    { href: "#", text: "Kontakt" },
+  ];
+
+  return (
+    <div className={className}>
+      {links.map((link, index) => (
+        <a
+          key={index}
+          href={link.href}
+          className="text-gold active:underline font-semibold"
+        >
+          {link.text}
+        </a>
+      ))}
+    </div>
+  );
+};
+
+export default NavLinks;
