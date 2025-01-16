a8abee3bc4ddb8a60ac88b12c1a723d586a28238
Karin Ekenberg <137770952+karinEkenberg@users.noreply.github.com>
2025-01-16 17:43:42 +0100

    accepted different changes in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,27 +7,18 @@ export default {
       extend: {
         fontFamily: {
         'inter': ['Inter', 'sans-serif'],
-<<<<<<< HEAD
-        'segoeui': ['Segoe UI', 'Arial', 'sans-serif'],
-=======
->>>>>>> main
         },
         colors: {
          sandybrown: "#C7A46C",
          springwood: "#FAF7F2", 
          coffee: "#AF7C47",
          sepiaskin: "#92633C",
-<<<<<<< HEAD
-         black: "#010101",
-         rangoongreen: "#1A1A1A",
-=======
          merino: "#faf7f2",
          wheat: "f5e2b8",
          brandy: "#e1c79e",
          codgray: "#1a1a1a",
          black: "#010101",
          mineshaft: "#333333",
->>>>>>> main
          lightgrey: "#D9D9D9",
          gold: '#C7A46C',
          darkgray: "#333333"
