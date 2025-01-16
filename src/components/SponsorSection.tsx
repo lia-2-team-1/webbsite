40c7572893d63a53c27b413ec6ffeb6328352bb1
DocSeattle <seattledraws@gmail.com>
2025-01-16 11:02:55 +0100

    -extra button +footer bg colour

--- src/components/SponsorSection.tsx
+++ src/components/SponsorSection.tsx
@@ -131,7 +131,6 @@ export default function SponsorSection() {
         </div>
         <div className="text-center">
         <Button text="bli sponsor" ></Button>
-        <Button isLink="true" text="bli sponsor" ></Button>
         </div>
         </div>
         </>
