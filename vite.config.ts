859dbbdfd242c2ec312ca94524c08b6645966e16
Karin Ekenberg <137770952+karinEkenberg@users.noreply.github.com>
2025-01-16 17:30:12 +0100

    accepted current change in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
@@ -19,17 +18,3 @@ export default defineConfig(({ mode }) => {
     },
   };
 });
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-        plugins: [tailwindcss()],
-    }
-  }
-})
->>>>>>> main
