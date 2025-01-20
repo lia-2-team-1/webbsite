d6dfc9dc663ac90d289bfe56673d43bedc80d7e4
DocSeattle <seattledraws@gmail.com>
2025-01-20 08:23:13 +0100

    refined component

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -11,57 +11,57 @@ export default function AboutUs({header, src}: AboutUsProps) {
     }
     return(
         <>
-                <h2 className="
-                    text-xl 
-                    text-center
-                    text-sandybrown
-                    font-bold 
-                    mt-3
-                ">{initProps.header}</h2>
+        <h2 className="
+        text-xl 
+        text-center
+        text-sandybrown
+        font-bold 
+        mt-3
+        ">{initProps.header}</h2>
         <div className="
-            w-screen
-            bg-mineshaft
-            flex flex-col md:flex-row-reverse
-            place-content-center
-            mx-auto p-3 md:gap-x-5
+        max-w-screen
+        bg-mineshaft
+        flex flex-col md:flex-row-reverse
+        place-content-center
+        mx-auto p-3 md:gap-x-5
         ">
-            <img 
-             src={`${initProps.src}`}
-             className="
-                max-w-md
-                lg:max-w-lg
-                mx-auto 
-                md:mx-0
-             "
-            /> 
-            <div className="
-                text-sandybrown
-                flex flex-col 
-                max-w-md 
-                lg:max-w-lg
-                mt-4
-                mx-auto md:mx-0
-                md:py-auto
-                md:mt-0
-            ">
-                <p className="
-                    text-base 
-                    md:my-auto
-                    md:pr-6
-                ">
-               I hjärtat av Borås blomstrar en nyskapad förening som inte bara siktar på att utmärka sig på planen, utan också strävar efter att vara en positiv kraft i samhället. Trandareds IF ska inte bara vara en idrottsförening, utan en plattform för engagemang, gemenskap och lokalt samhällsansvar. 
+        <img 
+        src={`${initProps.src}`}
+        className="
+        max-w-md
+        md:w-xl md:max-w-screen
+        mx-auto 
+        md:mx-0
+        "
+        /> 
+        <div className="
+        text-sandybrown
+        flex flex-col 
+        max-w-md 
+        md:w-xl md:max-w-screen
+        mt-4
+        mx-auto md:mx-0
+        md:py-auto
+        md:mt-0
+        ">
+        <p className="
+        text-base 
+        md:my-auto
+        md:pr-6
+        ">
+        I hjärtat av Borås blomstrar en nyskapad förening som inte bara siktar på att utmärka sig på planen, utan också strävar efter att vara en positiv kraft i samhället. Trandareds IF ska inte bara vara en idrottsförening, utan en plattform för engagemang, gemenskap och lokalt samhällsansvar. 
                 </p>
-                   <p className="text-base mt-4 md:my-auto md:pr-6">
-Välkommen till Trandareds IF, där vi inte bara spelar fotboll — vi förändrar liv.
-               </p>
-                <div className="
-                w-[100%]
-                flex flex-col
-                items-center
-                ">
-                <Button  isLink={true} text="Läs mer!"></Button>
-                </div>
-            </div>
+            <p className="text-base mt-4 md:my-auto md:pr-6">
+        Välkommen till Trandareds IF, där vi inte bara spelar fotboll — vi förändrar liv.
+                </p>
+            <div className="
+        w-full
+        flex flex-col
+        items-center
+        ">
+        <Button  isLink={true} text="Läs mer!"></Button>
+        </div>
+        </div>
         </div>
         </>
     )   
