60328a85f98154a060b3401c845105642c92444a
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-01-21 12:43:18 +0100

    remove unused codeline

--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -5,7 +5,7 @@ interface SectionHeadingProps {
   className?: string;
 }
 
-const SectionHeading: React.FC<SectionHeadingProps> = ({ text, className }) => {
+const SectionHeading: React.FC<SectionHeadingProps> = ({ text }) => {
   return (
     <h2 className="text-center lg:text-3xl text-2xl font-bold text-sandybrown">
       {text}
