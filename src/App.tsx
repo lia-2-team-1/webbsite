3f19ddd2f5fd96edffe802f7af9b7bf3a45a5f86
DocSeattle <seattledraws@gmail.com>
2025-01-21 10:04:22 +0100

    Cleaned up, sorted.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import SponsorSection from './components/SponsorSection.tsx'
-import './index.css'
-import Button from './components/Button.tsx'
-import Hero from './components/Hero.tsx'
-import Footer from './components/Footer.tsx'
-import AboutUs from './components/AboutUs.tsx'
+import './index.css';
+import React from 'react';
+import SponsorSection from './components/SponsorSection.tsx';
+import Button from './components/Button.tsx';
+import Hero from './components/Hero.tsx';
+import Footer from './components/Footer.tsx';
+import AboutUs from './components/AboutUs.tsx';
 import Navbar from "./components/Navbar.tsx";
+import LightDark from "./components/LightDark.tsx" ;
 
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = React.useState(0);
 
   return (
     <>
-      <div className="w-screen"></div>
       <div>
         <Navbar/>
         <Hero/>
