1fb8d5a96ac7faf21600732db51c148f04bc3e75
DocSeattle <seattledraws@gmail.com>
2025-01-31 08:27:48 +0100

    +SectionHeading styles -unneeded css file

--- src/components/SectionHeading.tsx
+++ src/components/SectionHeading.tsx
@@ -7,7 +7,7 @@ interface SectionHeadingProps {
 
 const SectionHeading: React.FC<SectionHeadingProps> = ({ text }) => {
   return (
-    <h2 className="text-center pt-5 pb-5 lg:text-3xl text-2xl font-bold text-sandybrown">
+    <h2 className="text-center pt-5 pb-5 lg:text-3xl text-2xl font-bold text-codgray dark:text-sandybrown">
       {text}
     </h2>
   );
