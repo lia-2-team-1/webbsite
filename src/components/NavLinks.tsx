1153e9c82c8d60c1998290bd21aa3aa43e1c2f1f
DocSeattle <seattledraws@gmail.com>
2025-02-03 15:20:12 +0100

    added login button and darkmode switch

--- src/components/NavLinks.tsx
+++ src/components/NavLinks.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-
+import LightDark from "./LightDark.tsx";
+import Button from "./Button.tsx";
 const NavLinks = ({ className }) => {
   const links = [
-    { href: "#", text: "Hem" },
     { href: "#", text: "Om oss" },
     { href: "#", text: "Schema" },
     { href: "#", text: "Nyheter" },
@@ -11,16 +11,20 @@ const NavLinks = ({ className }) => {
   ];
 
   return (
-    <div className={className}>
+    <div className={`${className}`}>
       {links.map((link, index) => (
         <a
           key={index}
           href={link.href}
-          className="text-codgray dark:text-sandybrown active:underline font-semibold"
+          className="
+          transition-[color] delay-150 duration-250 ease-out
+          text-codgray dark:text-sandybrown active:underline md:font-semibold md:text-nowrap hover:border-r-2 dark:border-sandybrown border-codgray md:hover:border-r-0 md:hover:border-b-2"
         >
           {link.text}
         </a>
       ))}
+      <Button text="Logga In" isLink={false} className="mx-0 md:mx-3 text-nowrap !ml-auto" />
+      <LightDark className="transition-[color_transform_position_display] delay-150 duration-250 ease-out"/>
     </div>
   );
 };
