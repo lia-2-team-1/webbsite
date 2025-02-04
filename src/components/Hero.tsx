1d1aa121418ce4db60bbf24a43b163df84b898e2
DocSeattle <seattledraws@gmail.com>
2025-02-04 13:08:18 +0100

    rewritten to be dynamic, flexible. Works with darkmode now.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,25 +3,48 @@ import Button from "./Button";
 
 export const Hero = () => {
   return (
-    <div className="w-full">
-      <div className="min-h-screen flex items-center justify-center bg-cover bg-no-repeat lg:bg-[url('/tif-hero.jpg')] bg-[url('/tif-hero-mobile.jpg')]">
-        <div className="bg-black bg-opacity-55 w-3/4 py-5 flex mx-auto lg:w-[35%] lg:text-center flex-col">
+      <div className="
+      h-screen w-full
+      flex flex-col place-items-center 
+      bg-cover bg-no-repeat md:bg-[url('/tif-hero.jpg')] bg-[url('/tif-hero-mobile.jpg')]">
+        <div className="
+            flex-col
+            py-5  my-auto 
+            w-3/4 md:w-3/5 
+            bg-sandybrown dark:bg-darkgray dark:bg-opacity-75 bg-opacity-75
+            md:text-center
+            text-nowrap
+            rounded-md
+        ">
+
           <h1
-            className="text-center text-4xl lg:text-6xl text-sandybrown font-bold 
+            className="text-center text-4xl lg:text-6xl text-mineshaft dark:text-sandybrown font-bold 
           px-1.5 lg:px-5"
           >
             Trandareds IF
           </h1>
-
-          <p className="text-sandybrown text-center font-medium">
+          <p className="text-mineshaft dark:text-sandybrown text-center font-medium">
             Engagemang, gemenskap och framtid
           </p>
         </div>
-        <div className="w-[80%] md:w-[40%] lg:w-[20%] bg-black bg-opacity-70 p-10 flex flex-col lg:items-center absolute lg:left-80 -bottom-14 left-10 right-10">
-          <Button isLink={true} linkTo={"#"} text={"STÖTTA OSS"} />
-          <Button isLink={true} linkTo="#" text="BLI MEDLEM" />
+        <div className="
+           flex-col
+            ptlr-3 
+            w-3/4 md:w-3/5
+        ">
+            <div className="
+                flex flex-col
+                p-4
+                pb-6
+                w-full md:w-2/5
+                bg-sandybrown dark:bg-darkgray bg-opacity-75 dark:bg-opacity-75 
+                text-nowrap
+                rounded-tr-lg rounded-tl-lg
+            ">
+                <Button text="Stötta Oss!" isLink={true}/>
+                <Button text="Bli Medlem!" isLink={true}/>
+            </div>
         </div>
-      </div>
     </div>
   );
 };
