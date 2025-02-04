009b45cb21f1f69993b070432b7aa2ba3d7dc315
DocSeattle <seattledraws@gmail.com>
2025-02-04 10:45:34 +0100

    +implemented certain changes as requested. Nav position to be determined

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,31 +22,33 @@ const Navbar = () => {
   return (
     <nav className="bg-sandybrown dark:bg-darkgray dark:border-gray-200 
                     sticky top-0
-                    flex h-20 gap-
+                    flex h-20 z-40 
                     justify-between
                     transition-all delay-150 duration-250 ease-out
                     ">
             <a
               href="#"
-              className="flex items-center rtl:space-x-reverse w-fit pr-[3.3rem]"
+              className="flex items-center"
             >
               <img src="logo.webp" className="w-20" alt="TRANDAREDS IF LOGO" />
-              <h2 className="text-codgray dark:text-sandybrown font-bold md:font-semibold whitespace-nowrap mr-[5px]"> TRANDAREDS IF | </h2>
               </a> 
+          <a className="text-center my-auto mr-auto">
+              <h2 className="text-codgray  dark:text-sandybrown font-bold md:font-semibold whitespace-nowrap mr-[5px]"> TRANDAREDS IF | </h2>
+          </a>
 
             <NavLinks className={`
                 bg-sandybrown dark:bg-darkgray
                 flex flex-col space-y-2 p-2 pt-4
-                absolute right-0 z-40
+                absolute right-0 z-30
                 transform ${isOpen ? "translate-y-[20%]" : "translate-y-[-100%]"}
-                transition-all delay-150 duration-250 ease-out
+                transition-all delay-150 duration-350 ease-in-out
                 rounded-bl-lg
 
                 md:transform-none md:translate-y-[0%] md:-translate-y-[0%] md:flex md:flex-row md:space-x-[1rem] md:space-y-0
                 md:static md:p-0 md:place-items-center md:bg-transparent dark:md:bg-transparent md:w-screen 
             `}/>
             <div onClick={toggleMenu}> 
-              <Burger className="block absolute right-0 top-[10px] md:hidden w-20 z-50" stroke="stroke-codgray dark:stroke-sandybrown" /> 
+              <Burger className="block absolute right-0 h-20 p-2 md:hidden z-50 dark:bg-mineshaft bg-sandybrown" stroke="stroke-codgray dark:stroke-sandybrown" /> 
             </div>
     </nav>
   );
