6063647a274a4971719c2c212fa284bfe8cb2b25
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-02-24 11:16:53 +0100

    add heading and error to futurematches

--- src/components/FutureMatches.tsx
+++ src/components/FutureMatches.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import LoadingAndErrorMatch from "./LoadingAndErrorMatch";
+import SectionHeading from "./SectionHeading";
 
 export default function FutureMatches() {
+  const loading = true;
+  const error = null;
   return (
-    <div>FutureMatches</div>
-  )
+    <div className="bg-brandy dark:bg-mineshaft">
+      <SectionHeading text="Kommande matcher" />
+      <LoadingAndErrorMatch loading={loading} error={error} />
+    </div>
+  );
 }
