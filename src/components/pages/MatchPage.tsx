548840fd4aabf3dc556ae04d716d51855b95e332
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-02-24 11:04:33 +0100

    remove players.tsx since playerinfo is not available in the api

--- src/components/pages/MatchPage.tsx
+++ src/components/pages/MatchPage.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import FutureMatches from "../FutureMatches";
-import Players from "../Players";
 import Match from "../Match";
 
 export default function MatchPage() {
   return (
     <>
       <Match />
-      <Players />
       <FutureMatches />
     </>
   );
