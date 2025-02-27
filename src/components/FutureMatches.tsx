677d83d1aae651199fe02a2688080f03c0a882fd
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-02-27 10:01:15 +0100

    change var to plats, add method to show date properly

--- src/components/FutureMatches.tsx
+++ src/components/FutureMatches.tsx
@@ -70,7 +70,7 @@ const FutureMatches = () => {
   };
 
   return (
-    <div className="bg-brandy dark:bg-mineshaft text-black dark:text-sandybrown grid grid-cols-1 gap-5">
+    <div className="bg-wheat dark:bg-codgray text-black dark:text-sandybrown grid grid-cols-1 gap-5">
       <SectionHeading text="Kommande matcher" />
       <LoadingAndErrorMatch loading={loading} error={error} />
       {!loading && !error && (
@@ -79,7 +79,7 @@ const FutureMatches = () => {
             visibleMatches.map((match) => (
               <div
                 key={match.gameId}
-                className="border-darkgrey border-2 m-auto rounded-lg w-4/5 lg:w-2/5 bg-wheat shadow-md hover:bg-brandy transition duration-300 dark:bg-mineshaft dark:hover:bg-gray-700"
+                className="border-darkgrey border-2 m-auto rounded-lg w-4/5 lg:w-2/5 bg-brandy shadow-md hover:bg-sandybrown transition duration-300 dark:bg-mineshaft dark:hover:bg-gray-700"
               >
                 <div className="flex-col p-5 m-auto">
                   <div className="flex justify-between items-center">
@@ -88,6 +88,39 @@ const FutureMatches = () => {
                       alt={match.homeTeamName}
                       className="w-20 h-16"
                     />
+                    <div className="text-center pt-5">
+                      <p>
+                        <strong>Tid:</strong> {match.time}
+                      </p>
+                      <p>
+                        <strong>Datum:</strong>{" "}
+                        {new Date(match.timeAsDateTime).toLocaleDateString(
+                          "sv-SE"
+                        )}
+                      </p>
+                      <p>
+                        <strong>Serie:</strong> {match.competitionName}
+                      </p>
+                      <p>
+                        <strong>Plats:</strong> {match.venueName},{" "}
+                        {match.venueLocality}
+                      </p>
+                      {match.isCancelled && (
+                        <p>
+                          <strong>Inställd:</strong> Ja
+                        </p>
+                      )}
+                      {match.isPostponed && (
+                        <p>
+                          <strong>Förskjuten:</strong> Ja
+                        </p>
+                      )}
+                      {match.isAbandoned && (
+                        <p>
+                          <strong>Match avbruten:</strong> Ja
+                        </p>
+                      )}
+                    </div>
                     <img
                       src={match.awayTeamImageUrl}
                       alt={match.awayTeamName}
@@ -97,36 +130,6 @@ const FutureMatches = () => {
                   <div className="flex justify-between items-center text-base md:text-lg lg:text-xl font-semibold">
                     <h3>{match.homeTeamName}</h3>
                     <h3>{match.awayTeamName}</h3>
-                  </div>
-                  <div className="text-center">
-                    <p>
-                      <strong>Tid:</strong> {match.time}
-                    </p>
-                    <p>
-                      <strong>Datum:</strong> {match.timeAsDateTime}
-                    </p>
-                    <p>
-                      <strong>Serie:</strong> {match.competitionName}
-                    </p>
-                    <p>
-                      <strong>Var:</strong> {match.venueName},{" "}
-                      {match.venueLocality}
-                    </p>
-                    {match.isCancelled && (
-                      <p>
-                        <strong>Inställd:</strong> Ja
-                      </p>
-                    )}
-                    {match.isPostponed && (
-                      <p>
-                        <strong>Förskjuten:</strong> Ja
-                      </p>
-                    )}
-                    {match.isAbandoned && (
-                      <p>
-                        <strong>Match avbruten:</strong> Ja
-                      </p>
-                    )}
                   </div>
                 </div>
               </div>
