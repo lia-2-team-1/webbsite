746c1c150e53edd42de08c32688119fbcf66d006
DocSeattle <seattledraws@gmail.com>
2025-03-11 09:43:09 +0100

    commented out import, fixed syntax errors

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,16 +3,17 @@ import { EmailTest } from './EmailTest';
 
 export const Hero = () => {
   return (
-    <div
-      className="
+    <>
+      <div
+        className="
       h-screen w-full
       flex flex-col place-items-center 
       bg-cover bg-no-repeat md:bg-[url('/tif-hero.jpg')] bg-[url('/tif-hero-mobile.jpg')]"
-    >
-      <EmailTest />
+      >
+        <EmailTest />
 
-      <div
-        className='
+        <div
+          className="
             flex-col
             py-5  my-auto 
             w-3/4 md:w-3/5 lg:w-2/5 
@@ -21,28 +22,23 @@ export const Hero = () => {
             text-nowrap
             rounded-md
         "
-      >
-        <h1
-          className="text-center text-4xl lg:text-6xl text-mineshaft dark:text-sandybrown font-bold 
+        >
+          <h1
+            className="text-center text-4xl lg:text-6xl text-mineshaft dark:text-sandybrown font-bold 
 
           px-1.5 lg:px-5"
-        >
-          Trandareds IF
-        </h1>
+          >
+            Trandareds IF
+          </h1>
 
-        <p className="text-mineshaft dark:text-sandybrown text-center font-medium">
-          Engagemang, gemenskap och framtid
-        </p>
-      </div>
-      <div
-        className='
-           flex-col
-            ptlr-3 
-            w-3/4 md:w-3/5
-        '
-      >
+          <p className="text-mineshaft dark:text-sandybrown text-center font-medium">
+            Engagemang, gemenskap och framtid
+          </p>
+        </div>
         <div
-          className='
+          className="flex-col ptlr-3 w-3/4 md:w-3/5">
+          <div
+            className="
                 flex flex-col
                 p-4
                 pb-6
@@ -50,13 +46,14 @@ export const Hero = () => {
                 bg-sandybrown dark:bg-darkgray bg-opacity-75 dark:bg-opacity-75 
                 text-nowrap
                 rounded-tr-lg rounded-tl-lg
-            '
-        >
-          <Button text='Stötta Oss!' isLink={true} />
-          <Button text='Bli Medlem!' isLink={true} />
-        </div>
-      </div>
-    </div>
+            "
+          >
+            <Button text='Stötta Oss!' isLink={true} />
+            <Button text='Bli Medlem!' isLink={true} />
+          </div>
+        </div >
+      </div >
+    </>
   );
 };
 
