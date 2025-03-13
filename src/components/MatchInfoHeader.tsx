6b61061b6b71ef67c8e1a51ffd146972bf7d88e1
Karin Ekenberg <k.ekenberg.dev@gmail.com>
2025-03-13 11:14:59 +0100

    add section to matchpage

--- src/components/MatchInfoHeader.tsx
+++ src/components/MatchInfoHeader.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import PageHeading from "./PageHeading";
 
 export default function MatchInfoHeader() {
   return (
-    <div>
-      <h1>Match Info</h1>
+    <div className="bg-wheat dark:bg-codgray p-5">
+      <PageHeading text="Matchinformation" />
+      <div className="text-mineshaft dark:text-sandybrown flex flex-col pb-5 pt-5 mx-auto w-4/5 md:w-3/5 lg:w-2/5 ">
+        <p>
+          Välkommen till Trandareds IF Matchinfo – här hittar du allt om våra
+          matcher! Från aktuella resultat till datum och platser för kommande
+          matcher, så är detta din plats för att hänga med i resan. Med
+          engagemang, gemenskap och passion tar vi oss an varje utmaning –
+          tillsammans går vi mot nya mål! ❤️⚽️
+        </p>
+        <img src="" alt="" />
+      </div>
     </div>
   );
 }
